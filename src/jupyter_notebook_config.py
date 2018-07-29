0646ee2cb6b108a332b0da06d4c5b543e4a28c3f
Carlos De Niz <carlitre@gmail.com>
2018-07-29 18:18:05 -0400

    Add files via upload

---
+++ src/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.open_browser = False
+c.NotebookApp.ip='*'
+c.NotebookApp.port = 8192
+c.NotebookApp.password = u'sha1:45f7d7ac038c:c36b98f22eac5921c435095af65a9a00b0e1eeb9'
+c.Authenticator.admin_users = {'jupyter'}
+c.LocalAuthenticator.create_system_users = True
