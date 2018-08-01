d0f85ae43ef108d10b96af9e19b7ddb396855923
Carlos De Niz <carlitre@gmail.com>
2018-08-01 10:14:58 -0400

    Updates hail version
    Run script to update hail to the latest version

---
+++ src/update_hail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export $HAIL_HOME=/opt/hail02-on-EMR
+cd $HAIL_HOME/src
+
+sudo rm -r hail
+sudo ./hail_build.sh
+
+for SLAVEIP in `sudo grep -i privateip /mnt/var/lib/info/*.txt | sort -u | cut -d "\"" -f 2` 
+do
+	scp /home/hadoop/hail-* $SLAVEIP:/home/hadoop/
+done
+
+sudo stop hadoop-yarn-resourcemanager; sleep 3; sudo start hadoop-yarn-resourcemanager
