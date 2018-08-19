f1e5c0c2d677b41e9976b679f04d829cf21fc913
Carlos De Niz <carlitre@gmail.com>
2018-08-19 19:57:58 -0400

    Update hail_build.sh

--- src/hail_build.sh
+++ src/hail_build.sh
@@ -45,11 +45,12 @@ if [ "$IS_MASTER" = true ]; then
 	
 	# src/scripts/context.py
 	sudo ln -s /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.171-7.b10.37.amzn1.x86_64/include /etc/alternatives/jre/include
-	
-  ./gradlew -Dspark.version=$SPARK_VERSION shadowJar archiveZip
 
-  cp $PWD/build/distributions/hail-python.zip $HOME
-  cp $PWD/build/libs/hail-all-spark.jar $HOME
+# Compile Spark 2.3.0
+./gradlew -Dspark.version=$SPARK_VERSION -Dbreeze.version=0.13.2 -Dpy4j.version=0.10.6 shadowJar archiveZip	
+#   ./gradlew -Dspark.version=$SPARK_VERSION shadowJar archiveZip
+#   cp $PWD/build/distributions/hail-python.zip $HOME
+#   cp $PWD/build/libs/hail-all-spark.jar $HOME
   
   echo "" >> $HOME/.bashrc
   echo "export PYTHONPATH=\${PYTHONPATH}:$HOME/hail-python.zip" >> $HOME/.bashrc
