b84778227849d82eae9e95462e5bccc94ea3a34a
Carlos De Niz <carlitre@gmail.com>
2018-08-20 01:37:47 -0400

    Add files via upload

--- src/update_hail.sh
+++ src/update_hail.sh
@@ -4,7 +4,13 @@ export $HAIL_HOME=/opt/hail-on-EMR
 cd $HAIL_HOME/src
 
 sudo rm -r hail
+sudo rm /etc/alternatives/jre/include/include
 sudo ./hail_build.sh
+
+cd hail
+
+cp $PWD/build/distributions/hail-python.zip $HOME
+cp $PWD/build/libs/hail-all-spark.jar $HOME
 
 for SLAVEIP in `sudo grep -i privateip /mnt/var/lib/info/*.txt | sort -u | cut -d "\"" -f 2` 
 do
