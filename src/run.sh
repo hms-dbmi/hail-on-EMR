b04bfbd6e760bcf6c4f387e8bff04bbb7ddcfc34
Carlos <carlos@MBI-PA-L08964.local>
2018-08-21 11:59:19 -0400

    pause before deploy

--- src/run.sh
+++ src/run.sh
@@ -52,9 +52,10 @@ else
 	echo "Using existing AWS credentials..."
 	echo "To reconfigure run: aws configure"
 	echo "For help visit: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html"
+	read -n 1 -s -r -p "Press any key to continue cloudformation"
 fi
 
-echo "Installing required packages..."
+echo "\n\nInstalling required packages..."
 pip install boto3 pandas botocore paramiko pyyaml -q #parallel-ssh 
 pip install -U pip -q 
 # pip uninstall -y greenlet -q
