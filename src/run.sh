103adcb01547a2d26370638d86147ca46a0b4452
Carlos <carlos@MBI-PA-L08964.local>
2018-08-21 18:22:48 -0400

    Added Linuxbrew

--- src/run.sh
+++ src/run.sh
@@ -2,15 +2,25 @@
 
 echo "Generating the EMR cluster. See log details at /tmp/cloudcreation_log.out"
 
-# Check if python3 is installed 
-BREW=$(which brew)
-	if [ -z "$BREW" ]; then
-		echo "\n\nInstalling Homebrew..."
-		sleep 1
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	fi
-PYTH==$(which python3)
+# Install brew
+OS=$(uname)
+if [ "$OS" == "Darwin" ]; then
+	echo "Installing brew for Mac"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+elif [ "$OS" == "Linux" ]; then
+	echo "Installing brew for Linux"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+		if [ ! -f ~/.bashrc ]; then # Fedora, Red Hat, CentOS, etc.
+			sudo yum -y groupinstall 'Development Tools'
+		elif [ ! -f ~/.bashrc ]; then # Debian, Ubuntu, etc.
+			sudo apt-get -y install build-essential
+		fi
+	echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >> ~/.bashrc
+	source ~/.bashrc
+fi
 
+# Check if python3 is installed 
+PYTH=$(which python3)
 if [ -z "$PYTH" ]; then
 	echo "\n\nInstalling Python3..."
 	sleep 1
