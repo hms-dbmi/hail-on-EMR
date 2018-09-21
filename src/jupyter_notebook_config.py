f23be8c3db280447ceac6a88fd8d957ddd7db9fd
biocdeniz <carlitre@gmail.com>
2018-09-21 09:08:17 -0400

    Update jupyter configuration

--- src/jupyter_notebook_config.py
+++ src/jupyter_notebook_config.py
@@ -1,5 +1,5 @@
 c.NotebookApp.open_browser = False
-c.NotebookApp.ip='*'
+c.NotebookApp.ip='0.0.0.0' #'*'
 c.NotebookApp.port = 8192
 c.NotebookApp.password = u'sha1:45f7d7ac038c:c36b98f22eac5921c435095af65a9a00b0e1eeb9'
 c.Authenticator.admin_users = {'jupyter'}
