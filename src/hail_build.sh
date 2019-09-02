aa3deeab75be135735a9c684feeefd7a05766792
Jack Ow <owjl@gis.a-star.edu.sg>
2019-09-02 16:02:00 +0800

    bug on 5.24

--- src/hail_build.sh
+++ src/hail_build.sh
@@ -43,9 +43,8 @@ if [ "$IS_MASTER" = true ]; then
   # Fixes issue of missing lz4 
   sudo yum install -y lz4
   sudo yum install -y lz4-devel
-  git clone https://github.com/broadinstitute/hail.git
+  git clone --single-branch --branch 5.24  https://github.com/broadinstitute/hail.git
   cd hail/hail/
-  git checkout 5.24
 	
 	# src/scripts/context.py
 	sudo ln -s /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.*.amzn1.x86_64/include /etc/alternatives/jre/include
