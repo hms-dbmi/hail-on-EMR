f7fd01d1c92eea687d6f4e17251157ae7fb25d30
Maxime HEBRARD <maxime.hebrard@gmail.com>
2019-09-23 16:04:03 +0800

    Add conda-env + Fix cluster_id issue + Fix yaml loader warning + Hide few prints + Update README

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # Hail 0.2 on Amazon EMR: `cloudformation` tool 
 
-This `cloudformation` tool creates an EMR cluster under an **emr-5.19.0** release using `Spark 2.3.2` and `Hadoop 2.8.3`, with  `Hail 0.2` and `JupyterNotebook` installed. 
+This `cloudformation` tool creates an EMR cluster under an **emr-5.24.0** release using `Spark 2.4.2` and `Hadoop 2.8.3`, with  `Hail 0.2` and `JupyterNotebook` installed. 
 
 ## Before using this tool (Prerequisites)
 
 This tool requires the following programs to be installed (if any of them is missing, they will be installed for you
 !): 
 
-* Python3 and pip (via Homebrew), including the necessary libraries
 * Amazon's `Command Line Interface (CLI)` utility
-* If there is no AWS account configured it will start the configuration for you. For additional help visit: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html 
+* `Conda` environment manager
 
 ## How to use this tool
 
@@ -19,28 +18,45 @@ a) **A valid EC2 key pair**. For additional details on how to create and use you
 
 b) **A configured `CLI` account**. If your `CLI` account has been previously configured, the tool will use it. If you want to re-configure a to use under a specific account or a different user, at the terminal type `aws configure`
 
+Then open your terminal and clone this repository: `git clone https://github.com/c-BIG/hail-on-EMR.git`.
+
+### Environment manager
+
+A conda environment file is shipped with the repository. It will ensure that additional dependencies are installed on your system.
+
+Create a conda environment based on `hail-on-EMR/src/conda-env.yaml` and activate it.
+
+```sh
+conda env create -f hail-on-EMR/src/conda-env.yaml
+conda activate hail
+```
+
 ### EMR `cloudformation`
 
-Open your terminal and clone this repository: `git clone https://github.com/hms-dbmi/hail-on-EMR`. 
- 
 1. `cd` into the `hail02-on-EMR/src` folder and with the text editor of your preference open the configuration file: `hail02_EMR.yaml`. This file will be used to provide information necessary to create the cluster. Fill in the fields as necessary using your personal key and security groups (SG) information and save your changes. See configuration details below:
 
 ```yaml
 config:
   EMR_CLUSTER_NAME: "my-hail-02-cluster" # Give a name to the EMR cluster
-  KEY_NAME: "my-key" # Input your key name DO NOT include the .pem extension
-  PATH_TO_KEY: "/full-path/to-key/" # Full path to .pem file
-  INSTANCE_TYPE: "c4.8xlarge" # Select the instance type, see table below. Or visit the instances types link in the FAQs section
-  CORE_COUNT: "3" # Number of cores. Additional reference in the EC2 FAQs website 
-  SUBNET_ID: "subnet-12345" # Select you private subnet. See the EC2 FAQs website
-  SLAVE_SECURITY_GROUP: "" # Creates a new group by default. You can also add a specific SG. See the SG link in the FAQs section
-  MASTER_SECURITY_GROUP: "" # Creates a new group by default. You can also add a specific SG. See the SG link in the FAQs section
   EC2_NAME_TAG: "my-hail-EMR" # Tags for the individual EC2 instances
   OWNER_TAG: "emr-owner" # EC2 owner tag
   PROJECT_TAG: "my-project" # Project tag
+  SUBPROJECT_TAG: "my-sub-project" # Sub-project tag
+  REGION: "ap-southeast-1" # AWS Region tag
   S3_BUCKET: "s3n://my-s3-bucket/" # Input your project's S3 bucket
+  KEY_NAME: "my-key" # Input your key name DO NOT include the .pem extension
+  PATH_TO_KEY: "/full-path/to-key/" # Full path to .pem file
+  MASTER_INSTANCE_TYPE: "c4.4xlarge" # Suggested EC2 instances, change as desired
+  SLAVE_INSTANCE_TYPE: "c4.4xlarge" # Suggested EC2 instances, change as desired
+  CORE_COUNT: "3" # Number of cores. Additional reference in the EC2 FAQs website
+  CORE_EBS_SIZE: "150" #
+  SUBNET_ID: "subnet-12345" # Select you private subnet. See the EC2 FAQs website
+  SLAVE_SECURITY_GROUP: "" # Creates a new group by default. You can also add a specific SG. See the SG link in the FAQs section
+  MASTER_SECURITY_GROUP: "" # Creates a new group by default. You can also add a specific SG. See the SG link in the FAQs section
+  RELEASE_LABEL: "emr-5.24.0"
 ```
-This script is defaulted to region `us-east-1`, instances `c4.8xlarge` : 3 `cores` and 1 `master`. For additional configuration details regarding the **emr-5.13.0** release, visit: <https://console.aws.amazon.com/elasticmapreduce/home?region=us-east-1#quick-create\:>. 
+
+This script is defaulted to region `ap-southeast-1`, instances `c4.4xlarge` : 3 `cores` and 1 `master`. For additional configuration details regarding the **emr-5.24.0** release, visit: <https://console.aws.amazon.com/elasticmapreduce/home?region=ap-southeast-1#quick-create\:>. 
 
 |Suggested **`INSTANCE_TYPE`s** |
 |:-------------------------:| 
@@ -57,12 +73,15 @@ This script is defaulted to region `us-east-1`, instances `c4.8xlarge` : 3 `core
 
 See additional instance details at: https://aws.amazon.com/ec2/instance-types/
 
-2. Execute the command: `sh hail_cloudformation_emr.sh`. The EMR creation takes between 5-7 minutes. The installation log file is located at `tail -f /tmp/cloudcreation_log.out`; the logs are available, under the same path, at both the local installation computer and at the master node of your EMR
-3. You can check the status of the EMR creation at: https://console.aws.amazon.com/elasticmapreduce/home?region=us-east-1. The EMR is successfully created once it gets the status `Waiting`. After created, allow ~20 minutes for all the programs to install. All the programs are installed automatically
-4. To obtain the **DNS** (to `ssh` in to the master node) and the **public IP** of the Master node (required to connect to the `JupyterNotebook`), from the terminal execute: 
+2. Execute the command: `python install2.py`. The EMR creation is initiated. The status of the cluster is monitored and will undergo from `STARTING` to `RUNNING` and `WAITING`. <!-- The EMR creation takes between 5-7 minutes. The installation log file is located at `tail -f /tmp/cloudcreation_log.out`; the logs are available, under the same path, at both the local installation computer and at the master node of your EMR -->
+3. Once the cluster is succesfully created and is `WAITING`, the installation of `HAIL` is initiated.
+<!-- You can check the status of the EMR creation at: https://console.aws.amazon.com/elasticmapreduce/home?region=ap-southeast-1. The EMR is successfully created once it gets the status `Waiting`. After created, allow ~20 minutes for all the programs to install. All the programs are installed automatically-->
+4. Once the installation of Hail is done, ssh to the master node using the `Master DNS` indicated by the script.
+<!-- To obtain the **DNS** (to `ssh` in to the master node) and the **public IP** of the Master node (required to connect to the `JupyterNotebook`), from the terminal execute: 
 ```bash
 tail -4 /tmp/cloudcreation_log.out | head -2
 ```
+-->
 
 ## Launching the `JupyterNotebook` with `Hail 0.2`
 
--- src/install2.py
+++ src/install2.py
@@ -49,21 +49,22 @@ def launch_emr(cluster_id, c):
     print('\nMaster DNS: '+ master_dns)
     print('Master IP: '+ master_IP)
 
+    print('Copying keys...')
     # Copy the key into the master
     command='scp -o \'StrictHostKeyChecking no\' -i '+c['config']['PATH_TO_KEY']+ '/' + c['config']['KEY_NAME']+'.pem '+c['config']['PATH_TO_KEY']+ '/' + c['config']['KEY_NAME']+'.pem hadoop@'+master_dns+':/home/hadoop/.ssh/id_rsa'
-    print (command)
+    # print (command)
     os.system(command)
 
     command='scp -o \'StrictHostKeyChecking no\' -i '+c['config']['PATH_TO_KEY']+ '/' + c['config']['KEY_NAME']+'.pem '+c['config']['PATH_TO_KEY']+ '/' + c['config']['KEY_NAME']+'.pem hadoop@'+master_dns+':/home/hadoop/'+ c['config']['KEY_NAME'] + '.pem'
-    print (command)
+    # print (command)
     os.system(command)
 
-    print('Copying keys...')
+    print('Copying installation script...')
     # Copy the installation script into the master
-    print('PATH:' + PATH)
+    # print('PATH:' + PATH)
     command='scp -o \'StrictHostKeyChecking no\' -i '+c['config']['PATH_TO_KEY']+ '/' + c['config']['KEY_NAME']+'.pem '+PATH+'/install_hail_python36.sh hadoop@'+master_dns+':/home/hadoop'
     command2='scp -o \'StrictHostKeyChecking no\' -i '+c['config']['PATH_TO_KEY']+ '/' + c['config']['KEY_NAME']+'.pem '+PATH+'/jupyter_pw hadoop@'+master_dns+':/home/hadoop/'
-    print (command)
+    # print (command)
     os.system(command)
     os.system(command2)
 
@@ -80,16 +81,18 @@ def launch_emr(cluster_id, c):
     # close the client connection
     client.close()
 
+    # END
+    print('DONE')
 
 def main(args):
 
-    c=yaml.load(open(PATH+"/hail02_EMR.yaml"))
+    c=yaml.load(open(PATH+"/hail02_EMR.yaml"), Loader=yaml.BaseLoader)
 
     # Create cluster - EBS Volume
     # command='aws emr create-cluster --applications Name=Ganglia Name=Spark Name=Zeppelin --tags \'Project='+c['config']['PROJECT_TAG']+'\' \'Owner='+c['config']['OWNER_TAG']+'\' \'Name='+c['config']['EC2_NAME_TAG']+'\' --ec2-attributes \'{"KeyName":"'+c['config']['KEY_NAME']+'","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"'+c['config']['SUBNET_ID']+'","EmrManagedSlaveSecurityGroup":"'+c['config']['SLAVE_SECURITY_GROUP']+'","EmrManagedMasterSecurityGroup":"'+c['config']['MASTER_SECURITY_GROUP']+'"}\' --service-role EMR_DefaultRole --release-label emr-5.19.0 --log-uri \''+c['config']['S3_BUCKET']+'\' --name \''+c['config']['EMR_CLUSTER_NAME']+'\' --instance-groups \'[{"InstanceCount":1,"EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":32,"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceGroupType":"MASTER","InstanceType":"'+c['config']['INSTANCE_TYPE']+'","Name":"Master Instance Group"},{"InstanceCount":'+c['config']['CORE_COUNT']+',"EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":32,"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceGroupType":"CORE","InstanceType":"'+c['config']['INSTANCE_TYPE']+'","Name":"Core Instance Group"}]\' --configurations \'[{"Classification":"spark","Properties":{"maximizeResourceAllocation":"true"},"Configurations":[]}]\' --scale-down-behavior TERMINATE_AT_TASK_COMPLETION --region ap-southeast-1'
 
     # Create Cluster - Instance Store
-    command='aws emr create-cluster --applications Name=Ganglia Name=Spark Name=Zeppelin Name=JupyterHub --tags \'project='+c['config']['PROJECT_TAG']+'\' \'Owner='+c['config']['OWNER_TAG']+'\' \'Name='+c['config']['EC2_NAME_TAG']+'\' --ec2-attributes \'{"KeyName":"'+c['config']['KEY_NAME']+'","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"'+c['config']['SUBNET_ID']+'","EmrManagedSlaveSecurityGroup":"'+c['config']['SLAVE_SECURITY_GROUP']+'","EmrManagedMasterSecurityGroup":"'+c['config']['MASTER_SECURITY_GROUP']+'"}\' --service-role EMR_DefaultRole --enable-debugging --release-label \''+c['config']['RELEASE_LABEL']+'\' --log-uri \''+c['config']['S3_BUCKET']+'\' --name \''+c['config']['EMR_CLUSTER_NAME']+'\' --instance-groups \'[{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"'+c['config']['MASTER_INSTANCE_TYPE']+'","Name":"Master Instance Group"},{"InstanceCount":'+c['config']['CORE_COUNT']+',"InstanceGroupType":"CORE","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":'+c['config']['CORE_EBS_SIZE']+',"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceType":"'+c['config']['CORE_INSTANCE_TYPE']+'","Name":"Core Instance Group"}]\' --configurations \'[{"Classification":"spark","Properties":{"maximizeResourceAllocation":"true"},"Configurations":[]}]\' --configurations \'[{"Classification":"livy-conf","Properties":{"livy.server.session.timeout":"8h"},"Configurations":[]}]\'  --ebs-root-volume-size 32  --scale-down-behavior TERMINATE_AT_TASK_COMPLETION --region ap-southeast-1'
+    command='aws emr create-cluster --applications Name=Ganglia Name=Spark Name=Zeppelin Name=JupyterHub --tags \'project='+c['config']['PROJECT_TAG']+'\' \'Owner='+c['config']['OWNER_TAG']+'\' \'Name='+c['config']['EC2_NAME_TAG']+'\' --ec2-attributes \'{"KeyName":"'+c['config']['KEY_NAME']+'","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"'+c['config']['SUBNET_ID']+'","EmrManagedSlaveSecurityGroup":"'+c['config']['SLAVE_SECURITY_GROUP']+'","EmrManagedMasterSecurityGroup":"'+c['config']['MASTER_SECURITY_GROUP']+'"}\' --service-role EMR_DefaultRole --enable-debugging --release-label \''+c['config']['RELEASE_LABEL']+'\' --log-uri \''+c['config']['S3_BUCKET']+'\' --name \''+c['config']['EMR_CLUSTER_NAME']+'\' --instance-groups \'[{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"'+c['config']['MASTER_INSTANCE_TYPE']+'","Name":"Master Instance Group"},{"InstanceCount":'+c['config']['CORE_COUNT']+',"InstanceGroupType":"CORE","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":'+c['config']['CORE_EBS_SIZE']+',"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceType":"'+c['config']['SLAVE_INSTANCE_TYPE']+'","Name":"Core Instance Group"}]\' --configurations \'[{"Classification":"spark","Properties":{"maximizeResourceAllocation":"true"},"Configurations":[]}]\' --configurations \'[{"Classification":"livy-conf","Properties":{"livy.server.session.timeout":"8h"},"Configurations":[]}]\'  --ebs-root-volume-size 32  --scale-down-behavior TERMINATE_AT_TASK_COMPLETION --region '+c['config']['REGION']
 
     print("\n\nYour AWS CLI export command:\n")
     print(command)
@@ -103,16 +106,17 @@ def main(args):
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
         cluster_id_byte = process.stdout.read()
-        cluster_id_json = cluster_id_byte.decode("utf-8")
-        cluster_id_dict = {}
-        cluster_id_dict = ast.literal_eval(cluster_id_json)
+        # cluster_id_json = cluster_id_byte.decode("utf-8")
+        # cluster_id_dict = {}
+        # print(cluster_id_json)
+        # cluster_id_dict = cluster_id_json
+        # print("cluster_id_dict")
+        # print( cluster_id_dict)
+        # cluster_id = cluster_id_dict["ClusterId"]
 
-        print("cluster_id_dict")
-        print( cluster_id_dict)
-        cluster_id = cluster_id_dict["ClusterId"]
+        cluster_id = cluster_id_byte.decode("utf-8").rstrip()
 
-
-    print( cluster_id)
+    print(cluster_id)
 
     launch_emr(cluster_id, c)
 
